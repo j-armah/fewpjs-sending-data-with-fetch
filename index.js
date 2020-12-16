c3d313c964a29cee5f3b0057832ccf4e65b4c085
februar-y <jarmah241@gmail.com>
2020-12-16 14:49:07 -0500

    commit tree 242d5e6f49e1e0295824cc97ed60a9a3b5d5cdfd

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+function submitData (name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name: name, email: email})
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data)
+        let body = document.body
+        body.append(data.id)
+    })
+    .catch(function(error) {
+        let body = document.body
+        body.append(error.message)
+    })
+}
